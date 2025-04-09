fdc60e6abbffe2ce400836aa3e43832fa04ad9ca
Choomper01 <charbeltrad456@gmail.com>
2025-04-09 03:36:45 +0000

    [🌙] Backup realizado! 2025-04-09

--- servidor_minecraft/kubejs/server_scripts/skin.js
+++ servidor_minecraft/kubejs/server_scripts/skin.js
@@ -1,47 +1,50 @@
-const skins = {
-    'Charbel': 'https://i.imgur.com/4hRSnYO.png',
-    'Jorge': 'https://i.imgur.com/4hRSnYO.png',
-    'Gustavo': 'https://i.imgur.com/4hRSnYO.png'
-}
+// Versión compatible con KubeJS antiguo
+var skins = {
+    'charbel': 'https://i.imgur.com/4hRSnYO.png',
+    'jorge': 'https://i.imgur.com/4hRSnYO.png',
+    'gustavo': 'https://i.imgur.com/4hRSnYO.png'
+};
 
-export function registerCommands(event) {
-    event.register('skin', 'Cambia tu skin a una predeterminada', builder => {
-        // Argumento con sugerencias de skins disponibles
+// Función principal
+function registerSkinCommand(event) {
+    event.register('skin', function(builder) {
+        builder.requires(function(source) {
+            return source.hasPermission(2); // Requiere permisos de OP
+        });
+        
+        // Versión con argumento
         builder.then(argument('nombre', StringArgumentType.string())
-            .suggests((context, suggestionsBuilder) => {
-                // Sugerir los nombres de skins disponibles
-                Object.keys(skins).forEach(skinName => {
-                    suggestionsBuilder.suggest(skinName)
-                })
-                return suggestionsBuilder.buildFuture()
+            .suggests(function(context, suggestions) {
+                Object.keys(skins).forEach(function(key) {
+                    suggestions.suggest(key);
+                });
+                return suggestions.buildFuture();
             })
-            .executes(context => {
-                const skinType = StringArgumentType.getString(context, 'nombre').toLowerCase()
-                const player = context.source.player
-                const playerName = player.name.string
+            .executes(function(context) {
+                var player = context.source.player;
+                var nombre = StringArgumentType.getString(context, 'nombre').toLowerCase();
                 
-                // Verificar si el tipo de skin existe
-                if (!skins[skinType]) {
-                    player.tell(Text.red('Skin no válida. Opciones disponibles: ' + Object.keys(skins).join(', ')))
-                    return 0
+                if (!skins[nombre]) {
+                    player.tell(Text.red('Nombre de skin no válido. Opciones: ' + Object.keys(skins).join(', ')));
+                    return 0;
                 }
                 
-                // Obtener la URL correspondiente
-                const skinUrl = skins[skinType]
-                
-                // Ejecutar el comando setskin
-                Utils.server.runCommandSilent(`setskin ${skinUrl} ${playerName}`)
-                
-                // Mensaje de confirmación
-                player.tell(Text.green(`Skin cambiada a la versión almacenada "${skinType}"`))
-                return 1
-            })
-        )
+                var result = Utils.server.runCommandSilent('setskin ' + skins[nombre] + ' @s');
+                if (result) {
+                    player.tell(Text.green('Skin cambiada a ' + nombre));
+                    return 1;
+                }
+                player.tell(Text.red('Error al cambiar skin'));
+                return 0;
+            }));
         
-        // Mensaje de ayuda si no se proporciona argumento
-        builder.executes(context => {
-            context.source.player.tell(Text.yellow('Uso: /skin <nombre> (Opciones: ' + Object.keys(skins).join(', ') + ')'))
-            return 0
-        })
-    })
+        // Versión sin argumento (muestra ayuda)
+        builder.executes(function(context) {
+            context.source.player.tell(Text.yellow('Usa: /skin <nombre> (' + Object.keys(skins).join(', ') + ')'));
+            return 0;
+        });
+    });
 }
+
+// Registro tradicional para KubeJS antiguo
+onEvent('command.registry', registerSkinCommand);
